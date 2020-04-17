02986539932fa7148ce6a08f1fcddf52fbae2fb7
Huze-UPS <heth@live.fr>
2020-04-17 18:11:27 +0200

    progress TD 02 Vue

--- lab/02/01/app.js
+++ lab/02/01/app.js
@@ -4,6 +4,30 @@ function initApp() {
         data: {
             categories,
             countries,
+            categorySelected: {},
+            itemSelected: {},
+            cartItems: [],
+            contact: {
+                firstName: "",
+                lastName: "",
+                adress: "",
+                
+            }
         },
+        computed: {
+            cartTotal: function() {
+                let total = 0;
+                this.cartItems.forEach(item => total += item.unitPrice);
+                return total;
+            }
+        },
+        methods: {
+            addToCart: function() {
+                this.cartItems.push(this.itemSelected);
+            },
+            removeFromCart: function(itemToRemove) {
+                this.carItems = this.cartItems.filter(item => itemToRemove.id !== item.id);
+            }
+        }
     });
 }
