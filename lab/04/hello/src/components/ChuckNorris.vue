1eb1f53cd50b82e00bf3e8d8ea39fc85305af405
Huze-UPS <heth@live.fr>
2020-04-22 16:52:39 +0200

    TD 04 Vue

---
+++ lab/04/hello/src/components/ChuckNorris.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Chuck Norris fact ...</h2>
+        <img src="../assets/chuck_norris.jpg" />
+        <br>
+        <h2>{{ fact }}</h2>
+    </div>
+</template>
+
+<script>
+ 
+    export default {
+        data: function() {
+            return {
+                fact: ""
+            }
+        },
+
+        props: {
+            facts: Array,
+        },
+
+        created() {
+            this.fact = this.facts[Math.floor(Math.random() * this.facts.length)];
+        },
+    }
+    
+</script>
